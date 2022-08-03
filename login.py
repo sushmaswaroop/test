269a96450b2a1acf0ce2770275887f7138259d5b
sushmaswaroop <109276442+sushmaswaroop@users.noreply.github.com>
2022-08-03 14:52:52 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,12 @@
+username = input(' enter your username: ')
+if username == 'Put Your Username Here':
+   print("checking username")
+
+   print("username is right") 
+   password = input(' type your password: ')
+   if password == 'Put Your Password Here':
+       print("yes")
+   else:
+       exit()
+else:
+ exit()    
